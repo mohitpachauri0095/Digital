4c8c67def7cdb015a266f7ae23441677c90bbe89
Deepak Kumar <dk1184366@gmail.com>
2026-04-21 17:24:59 +0530

    commit

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -5,5 +5,5 @@ const adminSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 }, { timestamps: true });
-
+module.exports = mongoose.model('Admin', adminSchema);commit
 
